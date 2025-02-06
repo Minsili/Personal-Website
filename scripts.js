7e620f2c1241001657ee831a74c8ff741f73277b
Minsili <dianeminou2@gmail.com>
2025-02-06 12:35:42 +0100

    Reviewed heading font-size on small screens

--- scripts.js
+++ scripts.js
@@ -1,22 +1,25 @@
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
+let navLinks = document.querySelectorAll(".navbar a");
 
 window.onscroll = () => {
+  let top = window.scrollY;
+
   sections.forEach((sec) => {
-    let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach((links) => {
-        links.classList.remove("active");
-        document
-          .querySelector("header nav a [href*=" + id + " ]")
-          .classList.add("active");
+      navLinks.forEach((link) => {
+        link.classList.remove("active"); // Remove active from all links
       });
+
+      let activeLink = document.querySelector(".navbar a[href=#" + id + "]");
+      if (activeLink) {
+        activeLink.classList.add("active"); // Add active to current link
+      }
     }
   });
 };
